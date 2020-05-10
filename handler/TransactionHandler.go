6159aaf5a5e77119303e30dd5961b234ebd0cc67
nyarlathotep <41333884+ny4rl4th0t3p@users.noreply.github.com>
2020-05-10 15:59:05 +0200

    transaction handler

--- handler/TransactionHandler.go
+++ handler/TransactionHandler.go
@@ -4,8 +4,6 @@ import (
 	"blockchainFromScratch/blockchain"
 	"blockchainFromScratch/datastore"
 	"encoding/json"
-	"fmt"
-	"github.com/google/uuid"
 	"net/http"
 )
 
@@ -18,18 +16,14 @@ type TransactionRequest struct {
 }
 
 func (th *TransactionHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-
 	var transaction datastore.Transaction
-
 	err := json.NewDecoder(r.Body).Decode(&transaction)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	transaction.TransactionId = uuid.New()
-
-	fmt.Printf("%v", transaction)
-	th.Chain.PendingTransactions = append(th.Chain.PendingTransactions, transaction)
+	th.Chain.AddTransaction(transaction)
+	//th.Chain.BroadcastTransaction(transaction)
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(th.Chain.PendingTransactions)
+	json.NewEncoder(w).Encode(transaction)
 }
